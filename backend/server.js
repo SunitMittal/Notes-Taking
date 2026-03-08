35e3220f9f15d9e50fd36094d7166cde6fd7cd47
Sunit Mittal <sunitmittal01@gmail.com>
2026-03-08 23:46:57 +0530

    Set default port to 6000 if not specified

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT||6000;
 app.listen(port, () => console.log(`Server is running on Port ${port}`));
 
 module.exports = app;
